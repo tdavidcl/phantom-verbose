323f37aee5c18f5294b271ac8af80a6b070cdff0
tdavidcl <timothee.davidcleri@gmail.com>
2024-04-01 12:28:15 +0200

    add reproducer

---
+++ reproducer.sh
@@ -0,0 +1,17 @@
+export SYSTEM=gfortran
+export OMP_NUM_THREADS=1
+
+rm -rf sedov
+
+mkdir sedov
+
+cp blast.setup sedov
+cp blast.in sedov
+cd sedov
+
+sh ../scripts/writemake.sh sedov > Makefile
+make IND_TIMESTEPS=no
+make setup IND_TIMESTEPS=no
+
+./phantomsetup blast > out_phantom_setup
+./phantom blast.in > out_phantom
